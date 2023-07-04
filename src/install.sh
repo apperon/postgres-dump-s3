08e8c65208bcccb3c5abc165d9136b9b52f6f5f0
Alexander Heinrich <alexander.heinrich@apperon.io>
2023-07-04 10:56:27 +0200

    initial commit

---
+++ src/install.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+set -eux
+set -o pipefail
+
+apk update
+
+# install pg_dump
+apk add postgresql-client
+
+# install gpg
+apk add gnupg
+
+apk add python3
+apk add py3-pip  # separate package on edge only
+pip3 install awscli
+
+# cleanup
+rm -rf /var/cache/apk/*
---
+++ src/run.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+set -eu
+
+if [ "$S3_S3V4" = "yes" ]; then
+  aws configure set default.s3.signature_version s3v4
+fi
+
+sh backup.sh
+
